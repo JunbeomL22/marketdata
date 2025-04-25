25e5345ffc69c72aaeeb21c247f1aa3bac9842d4
Junbeom Lee <junbeoml22@gmail.com>
2025-04-25 16:33:17 +0900

    nil

--- Code/bench_json.py
+++ Code/bench_json.py
@@ -9,20 +9,42 @@ def save_bench_json():
     # get today from "D2" cell as "YYYYMMDD" format
     dt = sheet.range('D2').value
     dt_str = dt.strftime('%Y%m%d')
-    df['date'] = dt_str
-    # convert issue_date, maturity, list_date, code_date all to "YYYYMMDD" format
-    df['issue_date'] = df['issue_date'].apply(lambda x: x if x in (pd.NaT, None) else x.strftime('%Y%m%d'))
-    df['maturity'] = df['maturity'].apply(lambda x: x if x in (pd.NaT, None) else x.strftime('%Y%m%d'))
-    df['list_date'] = df['list_date'].apply(lambda x: x if x in (pd.NaT, None) else x.strftime('%Y%m%d'))
-    df['code_date'] = df['code_date'].apply(lambda x: x if x in (pd.NaT, None) else x.strftime('%Y%m%d'))
-
+    df['date'] = int(dt_str)
     # drop if "bench_id" is empty
     df = df[df['bench_id'].notna()]
 
-    # save as bench_rev.json
-    # keep korean characters in the json file
-    df = df[['date', 'isin', 'issue_date', 'maturity', 'list_date', 'code_date', 'bench_id']]
-    df.to_json('D:/Projects/marketdata/bench_rev.json', orient='records', date_format='iso', indent=4, force_ascii=False)
+    # convert issue_date, maturity, list_date, code_date all to "YYYYMMDD" format
+    df['issue_date'] = df['issue_date'].apply(lambda x: x if x in (pd.NaT, None) else int(x.strftime('%Y%m%d')))
+    df['maturity'] = df['maturity'].apply(lambda x: x if x in (pd.NaT, None) else int(x.strftime('%Y%m%d')))
+    df['list_date'] = df['list_date'].apply(lambda x: x if x in (pd.NaT, None) else int(x.strftime('%Y%m%d')))
+    df['code_date'] = df['code_date'].apply(lambda x: x if x in (pd.NaT, None) else int(x.strftime('%Y%m%d')))
+
+    # select df that bench_id starts_from KTB_
+    ktb_df = df[df['bench_id'].str.startswith('KTB_')].copy()
+    # group by bench_id and add column start and end
+    # start is issue_date and the end is the None or min of issue dates among bigger than my issue date
+    ktb_df['start'] = ktb_df['issue_date']
+    # sort by bench_id and issue_date to get the next issue date
+    ktb_df.sort_values(['bench_id', 'issue_date'], inplace=True)
+    ktb_df['end'] = ktb_df.groupby('bench_id')['issue_date'].shift(-1)
+
+    other_df = df[~df['bench_id'].str.startswith('KTB_')].copy()
+    other_df['end'] = other_df['maturity']
+    other_df.sort_values(['bench_id', 'maturity'], inplace=True)
+    other_df['start'] = other_df.groupby('bench_id')['maturity'].shift(1)
+    
+    ktb_res = ktb_df[['date', 'isin', 'start', 'end', 'bench_id']].copy()
+    other_res = other_df[['date', 'isin', 'start', 'end', 'bench_id']].copy()
+
+    res = pd.concat([ktb_res, other_res], ignore_index=True)
+
+    # if start and end is not None, pd.Nat or NaN convert to int
+    res['start'] = res['start'].apply(lambda x: x if x in (pd.NaT, None) or pd.isna(x) else str(int(x)))
+    res['end'] = res['end'].apply(lambda x: x if x in (pd.NaT, None) or pd.isna(x) else str(int(x)))
+
+    #res['start'] = res['start'].astype(str)
+    #res['end'] = res['end'].astype(str)
+    res.to_json('D:/Projects/marketdata/bench_rev.json', orient='records', date_format='iso', indent=4, force_ascii=False)
 
 if __name__ == "__main__":
     xw.Book('D:/Projects/marketdata/MarketData.xlsm').set_mock_caller()
